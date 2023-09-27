085825562047fc23c5b2cc50c42170576d6df07b
santi <santi.ledes.99@gmail.com>
2023-09-27 18:11:02 -0300

    add method to delete an employee

--- src/main/java/com/training/emanager/repo/EmployeeRepo.java
+++ src/main/java/com/training/emanager/repo/EmployeeRepo.java
@@ -3,4 +3,6 @@ package com.training.emanager.repo;
 import com.training.emanager.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface EmployeeRepo extends JpaRepository<Employee, Long> { }
+public interface EmployeeRepo extends JpaRepository<Employee, Long> {
+    void deleteEmployeeById(Long id);
+}
