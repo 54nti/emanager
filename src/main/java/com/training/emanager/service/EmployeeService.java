52978f7a44e2331224ce2bc61f42971fdcc30416
santi <santi.ledes.99@gmail.com>
2023-09-27 18:35:09 -0300

    add EmployeeService class

---
+++ src/main/java/com/training/emanager/service/EmployeeService.java
@@ -0,0 +1,43 @@
+package com.training.emanager.service;
+
+import com.training.emanager.model.Employee;
+import com.training.emanager.repo.EmployeeRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+import com.training.emanager.exception.UserNotFoundException;
+
+@Service
+public class EmployeeService {
+    private final EmployeeRepo employeeRepo;
+
+    @Autowired
+    public EmployeeService(EmployeeRepo employeeRepo) {
+        this.employeeRepo = employeeRepo;
+    }
+
+    public Employee addEmployee(Employee e) {
+        e.setEmployeeCode(UUID.randomUUID().toString());
+        return employeeRepo.save(e);
+    }
+
+    public List<Employee> findAllEmployees() {
+        return employeeRepo.findAll();
+    }
+
+    public Employee updateEmdployee(Employee e) {
+        return employeeRepo.save(e);
+    }
+
+    public Employee findEmployeeById(Long id) {
+        return employeeRepo.findEmployeeById(id)
+                .orElseThrow(() -> new UserNotFoundException("user by id " + id + " was now found"));
+    }
+
+    public void deleteEmployee(Long id) {
+        employeeRepo.deleteEmployeeById(id);
+    }
+
+}
