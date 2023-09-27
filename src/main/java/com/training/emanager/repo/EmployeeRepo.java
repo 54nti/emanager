938e3165dafdcefbbd5ffbb1573b5fb36636b600
santi <santi.ledes.99@gmail.com>
2023-09-27 17:43:15 -0300

    add repository

---
+++ src/main/java/com/training/emanager/repo/EmployeeRepo.java
@@ -0,0 +1,6 @@
+package com.training.emanager.repo;
+
+import com.training.emanager.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepo extends JpaRepository<Employee, Long> { }
